f2727f209810090de197d8c9aa5401ee30176d5c
mengtianxin <mantianxin0301@126.com>
2022-11-22 22:04:38 +0800

    fix

--- entityLinkTool/settings.py
+++ entityLinkTool/settings.py
@@ -120,9 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = ["https://example.com",
-    "https://sub.example.com",
-    "http：// localhost：8080",
-    "http://127.0.0.1:9000"]
+    "https://sub.example.com"]
 	
 CORS_ALLOW_METHODS = (
 	'DELETE',
