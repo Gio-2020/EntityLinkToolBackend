a7b9992ba6546dde8c0b860082f6458b9b615d0b
Gio-2020 <mantianxin0301@126.com>
2022-12-14 08:51:33 +0000

    try to fix

--- temp/admin.py
+++ temp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import KB, TrainingSet
+from .models import KB, TrainingSet, TrainingRecord
 # Register your models here.
 admin.site.register(KB)
 admin.site.register(TrainingSet)
+admin.site.register(TrainingRecord)
